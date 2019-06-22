a16420988d72926fb691e5116a90348fc307133e
Ivan Leskin <leskin.in@arenadata.io>
2019-06-22 18:12:41 +0300

    Fix apps.py

--- Django/photohack/photohack/urls.py
+++ Django/photohack/photohack/urls.py
@@ -13,9 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
+from django.conf.urls import url
 from .api import upload
 
 urlpatterns = [
-    path(r'^upload$', upload)
+    url(r'^upload$', upload)
 ]
