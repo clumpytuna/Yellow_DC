0be55c19d8344c9c1a54a7a0ae9c2e00f57b57f1
Ivan Leskin <leskin.in@arenadata.io>
2019-06-22 19:48:07 +0300

    Add download

--- Django/photohack/photohack/urls.py
+++ Django/photohack/photohack/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from .api_picture import upload
+from .api_picture import upload, download
 
 urlpatterns = [
-    url(r'^upload$', upload)
+    url(r'^upload$', upload),
+    url(r'^download$', download),
 ]
