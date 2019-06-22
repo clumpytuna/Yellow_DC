41d36649c5c54b874cd3e4101704597bbd2ab1de
Ivan Leskin <leskin.in@arenadata.io>
2019-06-22 19:57:25 +0300

    Enable media file serves

--- Django/photohack/photohack/urls.py
+++ Django/photohack/photohack/urls.py
@@ -1,22 +1,10 @@
-"""photohack URL Configuration
+from django.conf import settings
+from django.conf.urls.static import static
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.conf.urls import url
 from .api_picture import upload, download
 
 urlpatterns = [
     url(r'^upload$', upload),
     url(r'^download$', download),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
