84b548d2d39911c87654e8b4734c885405e032c7
Teodor Moiseev <femoiseev@gmail.com>
2019-06-23 10:38:48 +0300

    Fix Serega's API

--- Keypoints_API/get_keypoints.py
+++ Keypoints_API/get_keypoints.py
@@ -63,11 +63,11 @@ def apply_mask(image, mask, color, alpha=1.0):
     """Apply the given mask to the image.
     """
     for c in range(3):
-        image[:, :, c] = np.where(with open('data.json', 'r') as fp:
-    data = json.load(fp)mask == 1,
-                                  image[:, :, c] *
-                                  (1 - alpha) + alpha * color[c] * 255,
-                                  image[:, :, c])
+        image[:, :, c] = np.where(
+               mask == 1,
+               image[:, :, c] *
+               (1 - alpha) + alpha * color[c] * 255,
+               image[:, :, c])
     return image
 
 def transform_img_with_keypoints(image, keypoints_dataset='WFLW', device='cpu'):
