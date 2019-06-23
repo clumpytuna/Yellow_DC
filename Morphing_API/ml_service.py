43e07924b5486e8698baa27e730519474127ba47
Ivan Leskin <leskin.in@arenadata.io>
2019-06-23 17:07:49 +0300

    Add ML launch script

---
+++ Morphing_API/ml_service.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+import pika
+from json import loads, dumps
+
+from datetime import datetime
+
+from morphing import face_to_fruits
+
+
+QUEUE_IMAGES = 'images'
+QUEUE_RESULT = 'result'
+
+connection = None
+channel_send = None
+channel_receive = None
+
+
+def _prepare():
+    """
+    Prepare pika connection
+    """
+    global connection
+    global channel_send
+    global channel_receive
+
+    if connection is not None:
+        return
+
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+
+    channel_receive = connection.channel()
+    channel_receive.queue_declare(queue=QUEUE_IMAGES)
+
+    channel_send = connection.channel()
+    channel_send.queue_declare(queue=QUEUE_RESULT)
+
+
+def _callback(channel, method, properties, body):
+    r = loads(body)
+    print("RECIEVED image #{} with path {}".format(r['id'], r['path']))
+
+    resulting_path = "/tmp/{}_{}.{}".format(str(datetime.now()), r['id'], r['path'].split('.')[-1])
+    face_to_fruits(r['path'], resulting_path)
+
+    channel_send.basic_publish(
+            exchange='',
+            routing_key=QUEUE_RESULT,
+            body=dumps({'id': r['id'], 'path': resulting_path})
+    )
+    print("SENT image #{} with path {}".format(r['id'], resulting_path))
+
+
+_prepare()
+
+channel_receive.basic_consume(_callback, queue=QUEUE_IMAGES, no_ack=True)
+
+channel_receive.start_consuming()
---
+++ Morphing_API/run_ml.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+python3 ml_service.py
